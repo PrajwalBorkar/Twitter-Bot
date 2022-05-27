bda5e4352911ee7a26207a215f55b5172696a42c
Prajwal Borkar <48290911+PrajwalBorkar@users.noreply.github.com>
2022-05-27 13:54:29 +0530

    Update twitterClient.js

--- twitterClient.js
+++ twitterClient.js
@@ -1,10 +1,10 @@
 const {TwitterApi} = require("twitter-api-v2");
 
 const client = new TwitterApi( {
-    appKey:"8THvFTR6KUJl1Cla8l9cN6yQt",
-    appSecret:"kE4Gf6ngQmzWMd1jUXRDVqtHmNamoQv5ZSDoWRk541GbNDXH0S",
-    accessToken:"901783619147931648-pPIZgOB7851pbyDxcYv1bzqTHmGbwcS",
-    accessSecret:"bbgkbACUdgdx2ZthN5KiEwCId0FJ6pV9f75T9gbWOYI3l"
+    appKey:"YOUR PROJECT APPKEY",
+    appSecret:"YOUR PROJECT APPSECRET",
+    accessToken:"YOUR PROJCT ACCESS TOKEN",
+    accessSecret:"YOUR PROJECT ACCESS SECRET"
 })
 
 const rwClient = client.readWrite
